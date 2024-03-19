9d6e2cb0bef962c52cba1440ebe49186995908fc
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-03-19 16:21:44 +0530

    added main

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+import Home from './components/Home.vue'
+import AppFooter from './components/AppFooter.vue'
+import Services from './components/Services.vue'
+import Navbar from './components/Navbar.vue'
+import ContactUs from './components/ContactUs.vue'
+import About from './components/About.vue'
+</script>
+
+<template>
+  <header>
+    <Navbar/>
+  </header>
+  <main>
+    <section id="home">
+      <Home />
+    </section>
+    <section id="services">
+      <Services />
+    </section>
+    <section id="about">
+      <About />
+    </section>
+    <section id="contactUs">
+      <ContactUs />
+    </section>
+    <footer>
+      <AppFooter />
+    </footer>
+  </main>
+  
+</template>
+
+<style scoped>
+header {
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+
+footer {
+  width: 100vw;
+  bottom: 0;
+  left: 0;
+  z-index: 100;
+}
+
+section {
+  flex: 1;
+  display: flex;
+  min-height: 100vh;
+  width: 100vw;
+  overflow-y: hidden;
+}
+
+main {
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  left: 0;
+  top: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+  }
+
+
+  header .wrapper {
+    display: flex;
+  }
+}
+</style>
