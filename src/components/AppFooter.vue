57dd9efcfe8cccfe449575df9ad02d071c4f1bad
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-03-19 16:21:56 +0530

    added navbar and footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="footer">
+        <div class="section">
+            <h3>Address</h3>
+            <p>32/406, Sector 3, near KV-6 Basketball Court, Sanganer, Sector 3, Pratap Nagar, Jaipur, Rajasthan 302033</p>
+        </div>
+        <div class="section">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113954.10540303911!2d75.6512787972656!3d26.80603360000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396dcb08b5e87831%3A0x53dfe8db4b684dee!2sShree%20Govind%20Tourism!5e0!3m2!1sen!2sin!4v1710845120452!5m2!1sen!2sin" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="section">
+            <h3>Office Hours</h3>
+            <p>Monday - Friday: 9am - 5pm</p>
+        </div>
+        <div class="section">
+            <h3>Links</h3>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="section">
+            <h3>Socials</h3>
+            <ul>
+                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.footer {
+    background-color: #f5f5f5;
+    padding: 20px;
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+    background-color: #451d03;
+}
+
+.section {
+    width: 20vw;
+    margin-bottom: 20px;
+   color: #fff;
+   display: flex;
+   flex-direction: column;
+}
+
+h3 {
+    font-size: 18px;
+    margin-bottom: 10px;
+    color: #fff;
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    color: #fff;
+    display: flex;
+   flex-direction: column;
+}
+
+li {
+    display: inline-block;
+    margin-right: 10px;
+    color: #fff;
+}
+
+a {
+    color: #fff;
+    text-decoration: none;
+}
+
+a:hover {
+    color: #666;
+}
+
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,93 @@
+<template>
+    <div :class="{'navbar': true, 'scrolled': scrollHeight > 40}">
+        <h1>Shree Govind Tourism</h1>
+        <ul>
+            <li><a href="#home" v-on:click="scroll">Home</a></li>
+            <li><a href="#about" v-on:click="about">About</a></li>
+            <li><a href="#services" v-on:click="services">Services</a></li>
+            <li><a href="#contact" v-on:click="contact">Contact Us</a></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    import { ref, onMounted } from 'vue';
+
+    const scrollHeight = ref(0);
+
+    const scroll = () => {
+        const homeElement = document.getElementById('home');
+        if (homeElement) {
+            homeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    };
+
+    const about = () => {
+        const aboutElement = document.getElementById('about');
+        if (aboutElement) {
+            aboutElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    };
+
+    const services = () => {
+        const servicesElement = document.getElementById('services');
+        if (servicesElement) {
+            servicesElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    };
+
+    const contact = () => {
+        const contactElement = document.getElementById('contact');
+        if (contactElement) {
+            contactElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    };
+
+    onMounted(() => {
+        window.addEventListener('scroll', () => {
+            scrollHeight.value = window.scrollY;
+        });
+    });
+</script>
+
+<style scoped>
+    .navbar {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1.5rem 4rem;
+        background-color: transparent;
+    }
+
+    .scrolled {
+        background-color: #00000099;
+        transition: background-color 0.5s;
+    }
+
+    .navbar h1 {
+        font-size: 2.5rem;
+        color: #fff;
+    }
+
+    .navbar ul {
+        display: flex;
+        list-style: none;
+        width: 50%;
+        justify-content: space-between;
+    }
+
+    .navbar li {
+        font-size: 2rem;
+    }
+
+    .navbar a {
+        text-decoration: none;
+        color: white;
+    }
+
+    .navbar a:hover {
+        background: transparent;
+        color: #ffffff99;
+    }
+</style>
